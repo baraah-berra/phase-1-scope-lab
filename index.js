e99fc09df75df74b75a1eee1252b543ef8923164
baraah-berra <baraahalsaidramadan@gmail.com>
2023-04-09 12:45:09 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+function returnCustomerName() {
+  return customerName;
+}
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// overwrites the best customer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
